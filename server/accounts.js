1a5b63af1a9cf2964c536fa83d879212f52192ef
Santiago Molina Orozco <santiagomolina@nebular.co>
2017-01-08 14:01:27 -0500

    Add twiter api keys

--- server/accounts.js
+++ server/accounts.js
@@ -2,8 +2,8 @@ ServiceConfiguration.configurations.upsert({
     service: "twitter"
 }, {
     $set: {
-        consumerKey: "KEY",
-        secret: "KEY",
+        consumerKey: "QOetM5Oz7v0ynuCXLSXk61ZCD",
+        secret: "Fth7uduAz888dUhljHSLRGtgqh7WjcS5LLWoVomh1rA8fqHtC0",
         loginStyle: "popup"
     }
 });
